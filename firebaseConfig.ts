507ab520197bafe7ccf4181122cd90a30744207c
shabatiq11 <154967706+shabatiq11@users.noreply.github.com>
2024-09-11 17:23:51 +0500

    Update firebaseConfig.ts

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,8 +1,1 @@
-export const firebaseConfig = {
-  apiKey: 'AIzaSyCATYcc3uuaLlWZf1qovXv6qmFj-4R2FKs',
-  authDomain: 'angularapp-b2fb5.firebaseapp.com',
-  projectId: 'angularapp-b2fb5',
-  storageBucket: 'angularapp-b2fb5.appspot.com',
-  messagingSenderId: '953758774931',
-  appId: '1:953758774931:web:6ce17583af0b96408c3f66',
-};
+
